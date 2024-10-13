cee3fa103e4d9faaf334510d6e3366eb9245ec59
Caleb Call <caleb@blockjoy.com>
2024-10-13 07:00:41 -0600

    Switch to mainnet and adjust settings, remove Docker and Prometheus configs

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -47,7 +47,7 @@ async def run_exporter():
 
 
 def main():
-    logger.add("logs/monitor.log",
+    logger.add("/var/log/agave/monitor.log",
                level=LOG_LEVEL,
                rotation="00:00",
                retention="6 days",
